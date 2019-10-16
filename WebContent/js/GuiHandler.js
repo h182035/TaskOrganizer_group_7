0ceee5f0204c5a42f701e08c41eb52b986f37d8a
Ole Martinus Rambech <olerambech@hotmail.com>
2019-10-16 13:56:23 +0200

    nesten alt av innerHtml er erstattet med nodes. brukerinput blir lagt i textnode

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -31,53 +31,88 @@ export default class GuiHandler {
   showTask(task) {
     const tasksDiv = document.getElementById("tasks");
     let stringOptions;
+    const selectElement = document.createElement('select')
+    const option1 = document.createElement('option')
+    option1.setAttribute('value', '0')
+    option1.setAttribute('selected', '')
+    option1.innerHTML = '&lt;Modify&gt;'
+    selectElement.appendChild(option1)
     for (status in this.allstatuses) {
-      stringOptions +=
-        '<option value="' +
-        this.allstatuses[status] +
-        '">' +
-        this.allstatuses[status] +
-        "</option>";
+      let option2 = document.createElement('option')
+      option2.setAttribute('value', this.allstatuses[status])
+      option2.innerHTML = this.allstatuses[status]
+      selectElement.appendChild(option2)
     }
 
     if (tasksDiv.getElementsByTagName("table").length == 0) {
       console.log(tasksDiv.getElementsByTagName("table").length + "if");
 
-      tasksDiv.innerHTML =
-        '<table><thead><tr><th>Task</th><th>Status</th></tr></thead> <tbody><tr id="' +
-        task.id +
-        '">' +
-        "<td>" +
-        task.title +
-        "</td>" +
-        "<td>" +
-        task.status +
-        "</td>" +
-        "<td>" +
-        "<select>" +
-        ' <option value="0" selected="">&lt;Modify&gt;</option>' +
-        stringOptions +
-        "</select>" +
-        "</td>" +
-        '<td><button type="button">Remove</button></td>' +
-        " </tr></tbody></table>";
+
+        const table = document.createElement('table')
+        const thead = document.createElement('thead')
+        const th1 = document.createElement('th')
+        const th2 = document.createElement('th')
+        const tbody = document.createElement('tbody')
+        const tr = document.createElement('tr')
+        const tr2 = document.createElement('tr')
+        const td = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+        const td4 = document.createElement('td');
+        const td5 = document.createElement('td')
+        const button = document.createElement('button')
+        button.setAttribute('type', 'button')
+        button.innerHTML = 'Remove'
+
+        th1.innerHTML = 'Task'
+        th2.innerHTML = 'Status'
+        tr.appendChild(th1)
+        tr.appendChild(th2)
+        thead.appendChild(tr)
+        table.appendChild(thead)
+        
+
+        const tasktitle = document.createTextNode(task.title)
+        const taskstatus = document.createTextNode(task.status)
+
+        tr2.setAttribute('id', task.id)
+        td2.appendChild(tasktitle)
+        td3.appendChild(taskstatus)
+        td4.appendChild(selectElement)
+        td5.appendChild(button)
+        tr2.appendChild(td2)
+        tr2.appendChild(td3)
+        tr2.appendChild(td4)
+        tr2.appendChild(td5)
+
+        tbody.appendChild(tr2)
+        table.appendChild(tbody)
+
+
+        tasksDiv.appendChild(table)
+        
     } else {
       const nyTask = document.createElement("tr");
       nyTask.setAttribute("id", task.id);
-      nyTask.innerHTML =
-        "<td>" +
-        task.title +
-        "</td>" +
-        "<td>" +
-        task.status +
-        "</td>" +
-        "<td>" +
-        "<select>" +
-        ' <option value="0" selected="">&lt;Modify&gt;</option>' +
-        stringOptions +
-        "</select>" +
-        "</td>" +
-        '<td><button type="button">Remove</button></td>';
+
+        const tdTitle = document.createElement('td')
+        const titleText = document.createTextNode(task.title)
+        tdTitle.appendChild(titleText)
+        const tdStatus = document.createElement('td')
+        const statusText = document.createTextNode(task.status)
+        tdStatus.appendChild(statusText)
+        const tdSelect = document.createElement('td')
+        tdSelect.appendChild(selectElement)
+        const tdButton = document.createElement('td')
+        const button = document.createElement('button')
+        button.setAttribute('type', 'button')
+        button.innerHTML = 'Remove'
+        tdButton.appendChild(button)
+
+        nyTask.appendChild(tdTitle)
+        nyTask.appendChild(tdStatus)
+        nyTask.appendChild(tdSelect)
+        nyTask.appendChild(tdButton)
       const list = tasksDiv.getElementsByTagName('tbody')[0];
       list.insertBefore(nyTask, list.childNodes[0])
 
